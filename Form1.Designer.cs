54fa6612e012ff5229b98002e3436acf4924efb1
Severstal-Metiz <severstal.2016@gmail.com>
2022-05-26 16:55:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbI = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisX2.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series4";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1208, 366);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1208, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbI
+            // 
+            this.tbI.Location = new System.Drawing.Point(1075, 93);
+            this.tbI.Name = "tbI";
+            this.tbI.Size = new System.Drawing.Size(136, 20);
+            this.tbI.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 471);
+            this.Controls.Add(this.tbI);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbI;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Math;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        double period = 1; //period v sec
+        int iterations = 1000;
+        double dt;
+
+        double cnstValue = 1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dt = period / iterations;
+            DataPointCollection gr1 = chart1.Series["Series1"].Points;
+            DataPointCollection gr2 = chart1.Series["Series2"].Points;
+            DataPointCollection gr3 = chart1.Series["Series3"].Points;
+            DataPointCollection gr4 = chart1.Series["Series4"].Points;
+            fun(gr1, 2, 0, 1,Sin);
+            fun(gr2, 2, 0, 1,Sin);
+            funMul(gr3,gr1, gr2);
+            //fun(gr3, 2, 0, 1, Sin);
+            tbI.Text = Integral(gr3,gr4).ToString();
+
+            chart1.Series["Series1"].Enabled = false;
+            chart1.Series["Series2"].Enabled = false;
+
+        }
+
+        double Integral(DataPointCollection points, DataPointCollection resul = null)
+        {
+            double res=0;
+            for (int i = 0; i < iterations; i++)
+            {
+                res +=  dt * points[i].YValues[0];
+                resul.AddXY(i * dt, res / period);
+            }
+            return res/period;
+        }
+
+        double Cnst(double _)
+        {
+            return cnstValue;
+        }
+
+        void funAdd(DataPointCollection res, DataPointCollection pointsA, DataPointCollection pointsB)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                res.AddXY((double)i * dt, pointsA[i].YValues[0] + pointsB[i].YValues[0]);
+            }
+        }
+
+        void funMul(DataPointCollection res, DataPointCollection pointsA, DataPointCollection pointsB)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                res.AddXY((double)i * dt, pointsA[i].YValues[0] * pointsB[i].YValues[0]);
+            }
+        }
+
+        void fun(DataPointCollection points, double freq, double phazaG, double amplitude, Func<double,double> func)
+        {
+            double w;
+            double x, y;
+            double phaza = phazaG / 360 * 2 * PI;
+            for (double t = 0; t <= period; t += dt)
+            {
+                x = t; w = 2 * PI * freq;
+                //y = Sin((t + phaza) * w) * amplitude;
+                y = func(t * w + phaza) * amplitude;
+                points.AddXY(x, y);
+            }
+        }
+       
+
+       
+    }
+}
